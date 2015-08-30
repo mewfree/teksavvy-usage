05e45ae580413ae349297b8b7857bc29eec981e9
mewfree <damien.gonot@gmail.com>
2015-08-30 18:35:09 -0400

    minimal setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# teksavvy-usage
+Simple Python script to get your TekSavvy (Canadian ISP). You have to edit the file and provide your own API key and data cap.
+
+Inspired by https://gist.github.com/tessus/967c9b903e58d974907d
+
+There are other solutions available on GitHub but this is the simpler and most readable one (~15 lines of code).
---
+++ teksavvy_usage3.py
@@ -0,0 +1,15 @@
+import requests
+
+APIKEY = 'YOUR-API-KEY-HERE'
+CAP = 'YOUR-DATA-CAP-HERE'
+
+r = requests.get('https://api.teksavvy.com/web/Usage/UsageSummaryRecords?$filter=IsCurrent%20eq%20true', headers={'TekSavvy-APIKEY': APIKEY})
+
+d = r.json()['value'][0]
+pd = d['OnPeakDownload']
+pu = d['OnPeakUpload']
+left = CAP - pd
+
+s = 'You have used '+str(pd)+'GB out of your '+str(CAP)+'GB limit'
+s += ' ('+str(left)+'GB left)'
+print(s)
